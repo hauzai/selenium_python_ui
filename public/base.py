82eb6dcc9ecb4a6238a40729005b2d8474e4d383
hauzai <1017946794@qq.com>
2020-05-16 16:45:26 +0800

    1.Page基类修改 2.login_page 测试登录子类

--- public/base.py
+++ public/base.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common import exceptions
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class Page:
@@ -67,18 +68,50 @@ class Page:
         等待元素加载完毕
         :param css: 定位器
         :param sec: 等待时间
-        :return: 加载完毕的元素
+        :return:element 加载完毕的元素
         """
         if self._css_check(css):
             try:
-                WebDriverWait(self.driver, sec, 0.5).until(EC.presence_of_element_located(css), "根据条件查找不到对应元素")
+                return WebDriverWait(self.driver, sec, 0.5).until(EC.presence_of_element_located(css), "根据条件查找不到对应元素")
             except exceptions.TimeoutException as e:
                 print(e)
+
+    def element_send_keys(self, css, text):
+        """
+        元素输入文本
+        :param css: 定位器
+        :param text: 输入的文本
+        :return:
+        """
+        element = self.element_wait(css)
+        element.send_keys(text)
+
+    def element_click(self,css):
+        """
+        点击元素
+        :param css: 定位器
+        :return:
+        """
+        element: WebElement = self.element_wait(css)
+        element.click()
+
+    def element_get_text(self,css):
+        """
+        获取元素内容
+        :param css:
+        :return:
+        """
+        element: WebElement = self.element_wait(css)
+        return element.text
+
+
+
 
     def _css_check(self, css):
         if css[0].strip() in ["id", "name", "class name", "link text", "xpath", "css selector"]:
             return True
         else:
+            print("Please enter the correct targeting elements,'id','name','class','link_text','xpath','css'.")
             return False
 
 
@@ -87,7 +120,11 @@ if __name__ == '__main__':
     test = Page(wb, "http://192.168.1.96:8082/passport")
     test.open()
     test.max_window()
-    login = test.get_element(("id", "login_name"))
-    login.send_keys("root")
-    test.close()
-    test.quit()
+    login_name = test.get_element(("id", "login_name"))
+    login_name.send_keys("root")
+    login_password = test.get_element(("id","login_pwd"))
+    login_password.send_keys("zaq12wsx")
+    login_button = test.element_wait(("test","login"))
+    login_button.click()
+    #test.close()
+    #test.quit()
